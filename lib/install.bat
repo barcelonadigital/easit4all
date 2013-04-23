ea4800d8ebb5f7c6457c4c3394511bc74a8ea854
xrafael <xavi.rafael@rutesadojo.com>
2013-04-23 16:19:44 +0200

    Add install script file to add local jar files

--- lib/install.bat
+++
@@ -1,3 +0,0 @@
-call mvn install:install-file -Dfile=spring-social-facebook-1.0.2.SNAPSHOT.jar -DgroupId=org.bdigital -DartifactId=spring-social-facebook -Dversion=1.0.2.SNAPSHOT -Dpackaging=jar
-call mvn install:install-file -Dfile=spring-social-gmail-0.0.1-SNAPSHOT.jar -DgroupId=org.bdigital -DartifactId=spring-social-gmail -Dversion=0.0.1-SNAPSHOT -Dpackaging=jar
-call mvn install:install-file -Dfile=spring-social-google-1.0.3.SNAPSHOT.jar -DgroupId=org.bdigital -DartifactId=spring-social-google -Dversion=1.0.3.SNAPSHOT -Dpackaging=jar
